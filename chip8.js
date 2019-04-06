eb500c1875e018125f7c924b299043415c726c3e
kirjavascript <snkenjoi@gmail.com>
2019-04-06 19:50:48 +0100

    better for loops

--- chip8.js
+++ chip8.js
@@ -11,7 +11,7 @@ K.splice(512,0,...[...atob(location.search.slice(1))].map(d=>d.charCodeAt()));
 onkeyup=e=>{i=J.indexOf(e.key);G[i]=0;P&&(V[x]=i,P=0)};
 onkeydown=e=>G[J.indexOf(e.key)]=1;
 setInterval(_=>{
-    for(i=0;9>i;i++){
+    for(i=0;9>i;i++)
         if(!P){
             switch(O=K[H]<< 8|K[H+1],x=(O&3840)>>8,y=(O&240)>>4,z=O&15,R=O&255,Q=O&4095,H+=2,O&61440){
                 case 0:
@@ -88,9 +88,7 @@ setInterval(_=>{
                 case 53248:
                     V[15]=0;
                     K.slice(I,z+I).map((a,b)=>{
-                        for(j=0;8>j;j++) {
-                            (a&128)>0&&(x0=V[x]+j,y0=V[y]+b,loc=(0>x0?x0+64:x0%64)+64*(0>y0?y0+32:y0%32),M[loc]^=1,M[loc]||(V[15]=1)),a<<=1;
-                        }
+                        for(j=0;8>j;j++)(a&128)>0&&(x0=V[x]+j,y0=V[y]+b,loc=(0>x0?x0+64:x0%64)+64*(0>y0?y0+32:y0%32),M[loc]^=1,M[loc]||(V[15]=1)),a<<=1;
                     });
                     break;
                 case 57344:
@@ -125,18 +123,13 @@ setInterval(_=>{
                             K.splice(I,3,...[...(''+V[x]).padStart(3,0)].map(d=>+d));
                             break;
                         case 85:
-                            for(i=0;i<=x;){
-                                K[I+i]=V[i++];
-                            }
+                            for(i=0;i<=x;)K[I+i]=V[i++];
                             break;
                         case 101:
-                            for(i=0;i<=x;){
-                                V[i]=K[I+i++];
-                            }
+                            for(i=0;i<=x;)V[i]=K[I+i++];
                     }
             }
         }
-    }
     c=a=>a+a&&[a.splice(0,64).map(d=>d?'░░':'██').join``,...c(a)];
     document.body.innerHTML='<pre>'+c([...M]).join`\n`;
     B&&B--;
