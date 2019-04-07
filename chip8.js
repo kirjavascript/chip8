66c8768b214f69774ae4cf5e1c76feba2e074687
kirjavascript <snkenjoi@gmail.com>
2019-04-07 18:31:17 +0100

    another character

--- chip8.js
+++ chip8.js
@@ -10,8 +10,9 @@ M=T.slice(0,2048);
 K.splice(H,0,...[...atob(location.search.slice(1))].map(d=>d.charCodeAt()));
 onkeyup=e=>{i=J.indexOf(e.key);G[i]=0;P&&(V[x]=i,P=0)};
 onkeydown=e=>G[J.indexOf(e.key)]=1;
+c=a=>a+a&&[a.splice(0,64).map(d=>d?'░░':'██').join``,...c(a)];
 setInterval(_=>{
-    for(L=9;L--;)
+    for(L=9;L--;document.body.innerHTML='<pre>'+c([...M]).join`\n`)
         !P&&(
             O=K[H]<< 8|K[H+1],x=(O&3840)>>8,y=(O&240)>>4,z=O&15,R=O&255,Q=O&4095,H+=2,
             [
@@ -78,8 +79,6 @@ setInterval(_=>{
                 },
             ][(O&61440)/4096]()
         );
-    c=a=>a+a&&[a.splice(0,64).map(d=>d?'░░':'██').join``,...c(a)];
-    document.body.innerHTML='<pre>'+c([...M]).join`\n`;
     B&&B--;
     D?(D--,S||(A=new AudioContext,S=A.createOscillator(),S.connect(A.destination),S.start())):S&&(S.stop(),S=0)
 },16)
